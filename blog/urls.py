c733a84933ef506150ef88cd23948c0cec0d9931
Damodar Vishwakarma <vishwakarmadamodar@gmail.com>
2022-03-15 04:31:46 +0530

    second commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,15 +4,17 @@ from blog.views import home
 from . import views
 
 urlpatterns = [
-    # path('', home, name='blog-home'),
+    path('', home, name='blog-home'),
     path('', views.PostListView.as_view(),
          name='blog-home'),
-    path('blog/create', views.PostCreateView.as_view(),
+    path('blog/create/', views.PostCreateView.as_view(),
          name='post-create'),
     path('blog/<int:pk>', views.PostDetailView.as_view(),
-         name='blog-home'),
+         name='post_detail'),
     path('blog/<int:pk>/update', views.PostUpdateView.as_view(),
          name='post-update'),
     path('blog/<int:pk>/delete', views.PostDeleteView.as_view(),
          name='post-delete'),
+    path("contact/", views.contact, name="ContactUs"),
+    path("about/", views.about, name="blog-about"),
 ]
